2bdeb0acc0a96aa62c50df72d942fbd952bf5e1a
ankit96804325 <ankitpacheriya2003@gmail.com>
2026-01-16 13:15:25 +0530

    Prepare project for Vercel deployment

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const expenseRoutes = require("./routes/expenseRoutes");
 
 const app = express();
 
-// MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/expenseTracker")
+// MongoDB
+mongoose.connect(process.env.MONGO_URL || "mongodb://127.0.0.1:27017/expenseTracker")
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
@@ -18,7 +18,11 @@ app.use(express.static("public"));
 // Routes
 app.use("/", expenseRoutes);
 
-// Server
-app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
-});
+// Local only
+if (!process.env.VERCEL) {
+    app.listen(3000, () => {
+        console.log("Server running at http://localhost:3000");
+    });
+}
+
+module.exports = app;
